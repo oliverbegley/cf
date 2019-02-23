4289af2c092d3c828c128c22168ec37f8a1467fd
Oliver <oliverbegley1@gmail.com>
2019-02-23 17:04:21 +0000

    not found page fixed

--- client/app/components/App/NotFound.js
+++ client/app/components/App/NotFound.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => (
-  <>
+  <div style={{ height:"100%"}}>
+  <div style={{  position: "absolute", top: "50%",left: "50%",transform: "translate(-50%, -50%)"}}>
     <h2>Page not found</h2>
 
-    <Link to="/">Go home</Link>
-  </>
+    <Link to="/">Return to homepage</Link>
+    </div>
+  </div>
 );
 
 export default NotFound;
